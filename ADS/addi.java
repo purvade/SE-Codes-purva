2abe63fbbf435352e7f400d9f4076af286b4377a
NickGhule <nikghule1@gmail.com>
2020-04-11 13:27:21 +0530

    Resolved the erros
    *added constructors
    *rewritten main function

--- ADS/addi.java
+++ ADS/addi.java
@@ -1,38 +1,65 @@
-mport java.util.Scanner;
-public class Additionpublic{      
-    Scanner sc = new Scanner(System.in);  
-     int num1 = sc.nextInt();   
-    int num2 = sc.nextInt();   
-    int sum=num1+num2;   
-    System.out.println("Addition public is: "+sum);        }
-class Additionprivate{      
-    Scanner sc = new Scanner(System.in);   
-    private int num3= sc.nextInt();   
-    private int num4= sc.nextInt();      
-    public int getnum1()  
-     {     return num3;   }     
-     public int getnum2()   
-    {     return num4;   }
-    int sum=getnum1()+getnum2();
-     System.out.println("Addition is: "+sum);
+import java.util.Scanner;
+class Additionpublic
+{
+    public int num1,num2;
+    Additionpublic()
+    {
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("sum of Public :: "+sum);
+    }
+}
+class Additionprivate
+{
+    private int num1,num2;
+    Additionprivate()
+    {
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+    }
+    public int getnum1()
+    {
+        return num1;
+    }
+    public int getnum2()
+    {
+        return num2;
+    }
 }
 
-class Additionprotected{  
-     protected int num5, num6;   
+class Additionprotected
+{
+    protected int num5, num6;
 }
-class Extended extends Additionprotected{  
-Scanner sc = new Scanner(System.in);   
-    int num5 = sc.nextInt();   
-    int num6 = sc.nextInt();   
-    int sum=num5+num6;   System.out.println("Addition is: "+sum);}
 
-public class AdditionTwoNumbers2 {
- public static void main(String[] args){
-    System.out.println("Enter two Number: ");
-     Additionpublic pub=new Additionpublic();
-     System.out.println("Enter two Number: ");
-      Additionprivate pri=new Additionprivate();
- System.out.println("Enter two Number: ");
- Extended pro=new Extended();                        
- }
- }
+class Extended extends Additionprotected
+{
+    public int sum;
+    Extended()
+    {
+        Scanner sc = new Scanner(System.in);
+        num5 = sc.nextInt();
+        num6 = sc.nextInt();
+        sum  = num5 + num6;
+        System.out.println("Sum of protected :: "+sum);
+    }
+}
+    
+
+class addi
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter two Numbers");
+        Additionprivate pri = new Additionprivate();
+        int sum = pri.getnum1()+ pri.getnum2();
+        System.out.println("Sum of private :: " +sum);
+        System.out.println("Enter two Numbers");
+        Extended pro = new Extended();
+        System.out.println("Enter two Numbers");
+        Additionpublic plu = new Additionpublic();
+    }
+}
